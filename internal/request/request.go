fb6409fda6ed4b810b5e728967f7df2cf9b574c4
jimmyvallejo <jrv1295@gmail.com>
2025-11-17 11:00:15 -0500

    Tests

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,89 @@
 package request
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+	"unicode"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+const supportedVersion = "1.1"
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+
+	read, err := io.ReadAll(reader)
+	if err != nil {
+		fmt.Printf("failed to read, err: %e", err)
+	}
+
+	s := string(read)
+
+	return parseRequestLine(s)
+
+}
+
+func parseRequestLine(readerString string) (*Request, error) {
+
+	lines := strings.Split(readerString, "\r\n")
+
+	line := lines[0]
+
+	parts := strings.Split(line, " ")
+
+	if len(parts) < 3 {
+		return nil, errors.New("line length is incorrect")
+	}
+
+	method := parts[0]
+	if !isAllUpperAlpha(method) {
+		return nil, errors.New("method is incorrect")
+	}
+
+	requestTarget := parts[1]
+
+	httpVersion := parts[2]
+
+	versionParts := strings.Split(httpVersion, "/")
+	version := versionParts[1]
+
+	if version != supportedVersion {
+		return nil, errors.New("version is incorrect")
+	}
+
+	requestLine := RequestLine{
+		HttpVersion:   version,
+		RequestTarget: requestTarget,
+		Method:        method,
+	}
+
+	request := Request{
+		RequestLine: requestLine,
+	}
+
+	return &request, nil
+
+}
+
+func isAllUpperAlpha(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+
+	for _, r := range s {
+		if !unicode.IsUpper(r) || !unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,45 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "The Testagen", "The Testagen")
+	// Test: Good GET Request line
+	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good GET Request line with path
+	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Invalid number of parts in request line
+	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Good POST Request line with path
+	r, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Invalid Version in request line
+	r, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/2.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+	require.Nil(t, r)
+	require.ErrorContains(t, err, "version is incorrect")
 }
