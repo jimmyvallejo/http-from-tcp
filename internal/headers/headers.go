ffe7eea5e4d3c3377f8be22ba71c3b7090ea41ad
jimmyvallejo <jrv1295@gmail.com>
2025-11-27 11:33:46 -0500

    Adding header parsing

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,14 +1,62 @@
 package headers
 
+import (
+	"bytes"
+	"errors"
+	"regexp"
+)
+
 type Headers map[string]string
 
 func NewHeaders() Headers {
 	return make(Headers)
 }
 
-//Suffix = parse full
-// Prefix = done 
+var headerNameRegex = regexp.MustCompile(`^[a-zA-Z0-9!#$%&'*+\-.^_` + "`" + `|~]+$`)
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 
+	contains := bytes.Contains(data, []byte("\r\n"))
+	if !contains {
+		return 0, false, nil
+	}
+
+	startsWith := bytes.HasPrefix(data, []byte("\r\n"))
+	if startsWith {
+		return 2, true, nil
+	}
+
+	trimmed := bytes.TrimSpace(data)
+
+	splitted := bytes.SplitN(trimmed, []byte(":"), 2)
+
+	toCheck := splitted[0]
+
+	if !isValidHeaderName(toCheck) {
+		return 0, false, errors.New("invalid header name")
+	}
+
+	fieldValue := bytes.TrimSpace(splitted[1])
+
+	if bytes.ContainsAny(toCheck, " ") {
+		return 0, false, errors.New("invalid format")
+	}
+
+	keyToLower := bytes.ToLower(toCheck)
+
+	_, exists := h[string(keyToLower)]
+	if exists {
+		h[string(keyToLower)] += ", " + string(fieldValue)
+	} else {
+		h[string(keyToLower)] = string(fieldValue)
+	}
+
+	idx := bytes.Index(data, []byte("\r\n"))
+
+	return idx + 2, false, nil
+
+}
+
+func isValidHeaderName(name []byte) bool {
+	return headerNameRegex.Match(name)
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -10,12 +10,20 @@ import (
 func TestHeaders(t *testing.T) {
 	// Test: Valid single header
 	headers := NewHeaders()
-	data := []byte("Host: localhost:42069\r\n\r\n")
+	data := []byte("HOST: localhost:42069\r\n\r\n")
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Invalid key
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
 	assert.False(t, done)
 
 	// Test: Invalid spacing header
@@ -25,4 +33,33 @@ func TestHeaders(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	// Test: Valid single header with extra whitspace
+	headers = NewHeaders()
+	data = []byte("Host:    localhost:42069        \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 34, n)
+	assert.False(t, done)
+
+	// Test Multiple Values
+
+	headers = NewHeaders()
+	data1 := []byte("Content-Type: application/json\r\n\r\n")
+	data2 := []byte("Authorization: Bearer token123\r\n\r\n")
+	data3 := []byte("Host: localhost:42069\r\n\r\n")
+	_, _, _ = headers.Parse(data1)
+	_, _, _ = headers.Parse(data2)
+	n, done, err = headers.Parse(data3)
+
+	require.NoError(t, err)
+	assert.False(t, done)
+	assert.Greater(t, n, 0)
+
+	assert.Equal(t, "application/json", headers["content-type"])
+	assert.Equal(t, "Bearer token123", headers["authorization"])
+	assert.Equal(t, "localhost:42069", headers["host"])
+
 }
